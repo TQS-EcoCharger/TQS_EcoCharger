15932a102b80fd1d983e345699624b4a6f7688ca
alof04 <afonso.ferreira@ua.pt>
2025-05-15 14:35:51 +0100

    added tags to see if tests show up

--- backend/src/test/java/pt/ua/tqs/ecocharger/ecocharger/service/AuthenticationServiceTest.java
+++ backend/src/test/java/pt/ua/tqs/ecocharger/ecocharger/service/AuthenticationServiceTest.java
@@ -1,6 +1,7 @@
 package pt.ua.tqs.ecocharger.ecocharger.service;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,6 +21,7 @@ class AuthenticationServiceTest {
 
     @Test
     @DisplayName("ET-49: Should login successfully")
+    @Tag("ET-49")
     void testSuccessfulLogin() {
         AuthResultDTO result = authService.authenticate("john@example.com", "123456");
         assertThat(result.isSuccess()).isTrue();
@@ -28,6 +30,7 @@ class AuthenticationServiceTest {
 
     @Test
     @DisplayName("ET-49: Should fail login with wrong password")
+    @Tag("ET-49")
     void testWrongPassword() {
         AuthResultDTO result = authService.authenticate("john@example.com", "wrongpass");
         assertThat(result.isSuccess()).isFalse();
@@ -36,6 +39,7 @@ class AuthenticationServiceTest {
 
     @Test
     @DisplayName("ET-49: Should fail login with wrong email")
+    @Tag("ET-49")
     void testDisabledUser() {
         AuthResultDTO result = authService.authenticate("bob@example.com", "bobpass");
         assertThat(result.isSuccess()).isFalse();
@@ -44,6 +48,7 @@ class AuthenticationServiceTest {
 
     @Test
     @DisplayName("ET-49: Should fail login with non-existent user")
+    @Tag("ET-49")
     void testNonExistentUser() {
         AuthResultDTO result = authService.authenticate("nobody@example.com", "nopass");
         assertThat(result.isSuccess()).isFalse();
