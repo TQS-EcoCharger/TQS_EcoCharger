a0d5d083223c71694cacc63ce4769ac04717b406
alof04 <afonso.ferreira@ua.pt>
2025-05-16 12:51:33 +0100

    ET-64 working login feature + adding functionalities to ci pipeline

--- backend/src/test/java/pt/ua/tqs/ecocharger/ecocharger/functional/steps/LoginSteps.java
+++ backend/src/test/java/pt/ua/tqs/ecocharger/ecocharger/functional/steps/LoginSteps.java
@@ -13,33 +13,37 @@ import java.time.Duration;
 public class LoginSteps {
 
     private WebDriver driver;
+    private WebDriverWait wait;
 
     @Given("I am on the login page")
     public void i_am_on_the_login_page() {
         driver = new ChromeDriver();
-        driver.get("http://localhost:5000/login");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get("http://localhost:5000/");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
     }
 
     @When("I enter {string} into the {string} field")
     public void i_enter_into_the_field(String value, String fieldId) {
-        WebElement input = driver.findElement(By.id(fieldId));
+        WebElement input = wait.until(ExpectedConditions.elementToBeClickable(By.id(fieldId)));
         input.clear();
         input.sendKeys(value);
     }
 
     @And("I click the {string}")
     public void i_click_the_button(String buttonId) {
-        WebElement button = driver.findElement(By.id(buttonId));
+        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id(buttonId)));
         button.click();
     }
 
     @Then("I should be redirected to the home page")
     public void i_should_be_redirected_to_the_home_page() {
-        new WebDriverWait(driver, Duration.ofSeconds(5))
-                .until(ExpectedConditions.urlContains("/home"));
+        wait.until(ExpectedConditions.urlContains("/home"));
 
         String currentUrl = driver.getCurrentUrl();
         assertTrue(currentUrl.endsWith("/home"), "Expected to be on /home but was on " + currentUrl);
+        
+        driver.quit();
     }
-
 }
