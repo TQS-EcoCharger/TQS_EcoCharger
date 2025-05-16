4c0a3919cfd26adb822c018cde9e8748477ffb02
alof04 <afonso.ferreira@ua.pt>
2025-05-16 12:02:57 +0100

    trying functional tests

--- backend/src/test/java/pt/ua/tqs/ecocharger/ecocharger/functional/CucumberTest.java
+++ backend/src/test/java/pt/ua/tqs/ecocharger/ecocharger/functional/CucumberTest.java
@@ -1,16 +1,17 @@
-package pt.ua.tqs.ecocharger.ecocharger.functional_tests;
+package pt.ua.tqs.ecocharger.ecocharger.functional;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import io.cucumber.core.options.Constants;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("pt/ua/tqs/ecocharger/ecocharger/features")
-@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "pt.ua.tqs.ecocharger.ecocharger.functional_tests.steps")
-public class CucumberTest {
-
-}
+@ConfigurationParameter(
+    key = GLUE_PROPERTY_NAME,
+    value = "pt.ua.tqs.ecocharger.ecocharger.functional.steps"
+)
+public class CucumberTest {}
--- backend/src/test/java/pt/ua/tqs/ecocharger/ecocharger/functional/steps/LoginSteps.java
+++ backend/src/test/java/pt/ua/tqs/ecocharger/ecocharger/functional/steps/LoginSteps.java
@@ -1,11 +1,10 @@
-package pt.ua.tqs.ecocharger.ecocharger.functional_tests.steps;
+package pt.ua.tqs.ecocharger.ecocharger.functional.steps;
 
 import io.cucumber.java.en.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,7 +36,7 @@ public class LoginSteps {
     @Then("I should be redirected to the home page")
     public void i_should_be_redirected_to_the_home_page() {
         new WebDriverWait(driver, Duration.ofSeconds(5))
-            .until(ExpectedConditions.urlContains("/home"));
+                .until(ExpectedConditions.urlContains("/home"));
 
         String currentUrl = driver.getCurrentUrl();
         assertTrue(currentUrl.endsWith("/home"), "Expected to be on /home but was on " + currentUrl);
