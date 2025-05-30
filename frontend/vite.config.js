21d2cce3389f4906aa52bcaff38d0457fafaf9b4
alof04 <afonso.ferreira@ua.pt>
2025-05-30 12:55:35 +0100

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',  
+    host: '0.0.0.0',
     port: 5000,
+    allowedHosts: ['deti-tqs-19.ua.pt'], // ✅ Aqui, no nível do `server`
     proxy: {
       '/api': {
         target: 'http://backend:8080',
-        allowedHosts: ['deti-tqs-19.ua.pt'],
         changeOrigin: true,
         secure: false,
       },
