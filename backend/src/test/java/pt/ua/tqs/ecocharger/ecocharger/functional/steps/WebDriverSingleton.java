c7fad6e32038c9ba6fea5a96d15830f357ddb114
alof04 <afonso.ferreira@ua.pt>
2025-06-04 16:10:36 +0100

    sry

--- backend/src/test/java/pt/ua/tqs/ecocharger/ecocharger/functional/steps/WebDriverSingleton.java
+++ backend/src/test/java/pt/ua/tqs/ecocharger/ecocharger/functional/steps/WebDriverSingleton.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import java.time.Duration;
 
 public class WebDriverSingleton {
