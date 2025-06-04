e17127a9e4fb8d8a20c675215a415418e6eda8cc
alof04 <afonso.ferreira@ua.pt>
2025-06-04 16:52:06 +0100

    .

--- backend/src/test/java/pt/ua/tqs/ecocharger/ecocharger/functional/steps/WebDriverSingleton.java
+++ backend/src/test/java/pt/ua/tqs/ecocharger/ecocharger/functional/steps/WebDriverSingleton.java
@@ -21,6 +21,7 @@ public static void initialize() {
     WebDriverManager.chromedriver().setup();
 
     ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless=new");
     options.addArguments("--no-sandbox");
     options.addArguments("--disable-dev-shm-usage");
     options.addArguments("--window-size=1920,1080");
