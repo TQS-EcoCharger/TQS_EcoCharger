58cfe72ecca3a1364fe625890b54b4d86bda0429
alof04 <afonso.ferreira@ua.pt>
2025-06-04 16:43:03 +0100

    one more time

--- backend/src/test/java/pt/ua/tqs/ecocharger/ecocharger/functional/steps/WebDriverSingleton.java
+++ backend/src/test/java/pt/ua/tqs/ecocharger/ecocharger/functional/steps/WebDriverSingleton.java
@@ -8,10 +8,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.time.Duration;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class WebDriverSingleton {
   private static WebDriver driver;
   private static WebDriverWait wait;
+
 
   public static void initialize() {
     if (driver == null) {
@@ -22,10 +25,18 @@ public class WebDriverSingleton {
       options.addArguments("--disable-dev-shm-usage");
       options.addArguments("--window-size=1920,1080");
 
+      try {
+        Path tempProfile = Files.createTempDirectory("chrome-profile");
+        options.addArguments("--user-data-dir=" + tempProfile.toAbsolutePath().toString());
+      } catch (Exception e) {
+        throw new RuntimeException("Failed to create temp user-data-dir", e);
+      }
+
       driver = new ChromeDriver(options);
       wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
   }
+
 
   public static WebDriver getDriver() {
     return driver;
