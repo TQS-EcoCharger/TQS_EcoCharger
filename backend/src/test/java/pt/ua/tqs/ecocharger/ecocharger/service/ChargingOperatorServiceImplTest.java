bf4fc583e1ed8e7d8978d7b472c96618a2cd60d3
Ricardo-Alexandre-Antunes <ricardo.alexandre.antunes@ua.pt>
2025-06-06 01:08:35 +0100

    ET-22 added teststo chargingoperatorservice

---
+++ backend/src/test/java/pt/ua/tqs/ecocharger/ecocharger/service/ChargingOperatorServiceImplTest.java
@@ -0,0 +1,56 @@
+package pt.ua.tqs.ecocharger.ecocharger.service;
+
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import pt.ua.tqs.ecocharger.ecocharger.repository.ChargingOperatorRepository;
+import pt.ua.tqs.ecocharger.ecocharger.models.ChargingOperator;
+
+@ExtendWith(MockitoExtension.class)
+public class ChargingOperatorServiceImplTest {
+
+    @Mock
+    private ChargingOperatorRepository chargingOperatorRepository;
+
+    @InjectMocks
+    private ChargingOperatorServiceImpl chargingOperatorService;
+
+    ChargingOperator chargingOperator;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        chargingOperator = new ChargingOperator(1L, "testOperator", "password", "Test Operator", true);
+        when(chargingOperatorRepository.findById(1L)).thenReturn(Optional.of(chargingOperator));
+        when(chargingOperatorRepository.existsById(1L)).thenReturn(true);
+
+    }
+
+    @Test
+    @DisplayName("Test getting Charging Operator by ID")
+    public void testGetChargingOperatorById() {
+        ChargingOperator result = chargingOperatorService.getChargingOperatorById(1L);
+        assert result != null;
+        assert result.getId().equals(1L);
+        assert result.getName().equals("Test Operator");
+    }
+
+    @Test
+    @DisplayName("Test finding out if id exists")
+    public void testChargingOperatorExists() {
+        boolean exists = chargingOperatorService.chargingOperatorExists(1L);
+        assert exists;
+        verify(chargingOperatorRepository, times(1)).existsById(1L);
+    }
+    
+}
